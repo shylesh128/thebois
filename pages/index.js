7cdd5121579ca362128ff69e51fb817db493aabb
Shylesh <shylesh.s@ansrsource.com>
2023-11-27 00:07:46 +0530

    Update API endpoint URL

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,9 @@ const Home = () => {
       const fetchTweets = async () => {
         try {
           setLoading(true);
-          const response = await fetch("http://localhost:3000/api/tweet");
+          const response = await fetch(
+            "https://shylesh.onrender.com/api/tweet"
+          );
 
           if (response.ok) {
             const data = await response.json();
